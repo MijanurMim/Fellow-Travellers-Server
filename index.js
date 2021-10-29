454b1015bc7eaf455609de1e5ec1ad8e2af18a96
MijanurMim <mjanurmim1@gmail.com>
2021-10-29 20:45:03 +0600

    mongodb connected

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const { MongoClient } = require("mongodb");
+require("dotenv").config();
+const cors = require("cors");
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// MiddleWare
+app.use(cors());
+app.use(express.json());
+
+// MongoDb Connection
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.kin6o.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+console.log(uri);
+
+async function run() {
+  try {
+  } finally {
+    // await client.close()
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Fellow Travellers server is running ");
+});
+
+app.listen(port, () => {
+  console.log("Server is Running at port ", port);
+});
