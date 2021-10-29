30da0629675556e666cbdcef3cb25e92ac897215
MijanurMim <mjanurmim1@gmail.com>
2021-10-30 03:08:56 +0600

    delete added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const { MongoClient } = require("mongodb");
+const ObjectId = require("mongodb").ObjectId;
 require("dotenv").config();
 const cors = require("cors");
 
@@ -24,6 +25,8 @@ async function run() {
     console.log("database connected to fellow");
     const database = client.db("fellow_Travellers");
     const packageCollection = database.collection("packages");
+    const serviceCollection = database.collection("services");
+    const membersCollection = database.collection("members");
     const bookedPackages = database.collection("booked_Packages");
 
     // ADD PACKAGES
@@ -32,6 +35,21 @@ async function run() {
       const result = await packageCollection.insertOne(req.body);
       console.log(result);
     });
+
+    // ADD SERVICES
+    app.post("/addServices", async (req, res) => {
+      console.log(req.body);
+      const result = await serviceCollection.insertOne(req.body);
+      console.log(result);
+    });
+
+    // ADD Members
+    app.post("/addMember", async (req, res) => {
+      console.log(req.body);
+      const result = await membersCollection.insertOne(req.body);
+      res.send(result);
+    });
+
     // Searched Packages
     app.get("/searchPackages", async (req, res) => {
       console.log(req.query.search);
@@ -42,6 +60,31 @@ async function run() {
         .toArray();
       res.send(result);
       console.log(result);
+    });
+
+    // Get ALL Members
+    app.get("/allMembers", async (req, res) => {
+      const result = await membersCollection.find({}).toArray();
+      res.send(result);
+    });
+    // Get ALL Packages
+    app.get("/allPackages", async (req, res) => {
+      const result = await packageCollection.find({}).toArray();
+      res.send(result);
+    });
+    // Get ALL Services
+    app.get("/allServices", async (req, res) => {
+      const result = await serviceCollection.find({}).toArray();
+      res.send(result);
+    });
+
+    // Delete Package
+    app.delete("/deletePackage/:id", async (req, res) => {
+      console.log(req.params.id);
+      const result = await packageCollection.deleteOne({
+        _id: ObjectId(req.params.id),
+      });
+      res.send(result);
     });
   } finally {
     // await client.close()
